d0914c47401c0d6a05b6a9ae0da76526a9bad75d
Arthur <arthur.dufour@epsi.fr>
2019-05-26 19:41:49 +0200

    added: pwa support

--- src/main.js
+++ src/main.js
@@ -1,21 +1,9 @@
 import Vue from 'vue'
-// import Axios from 'axios'
 import App from './App.vue'
 import router from './router'
-// import store from './store/index'
+import './registerServiceWorker'
 
-// Vue.config.productionTip = false
-
-// const token = localStorage.getItem('token')
-// Axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
-// Axios.defaults.headers.common['Target-URL'] = 'https://www.open-medicaments.fr/'
-
-// // replace default http module with axios
-// Vue.prototype.$http = Axios;
-
-// // create a global variable
-// Vue.prototype.$apiUrl = process.env.VUE_APP_API_URL
-// Vue.prototype.$proxyUrl = process.env.VUE_APP_PROXY_URL
+Vue.config.productionTip = false
 
 new Vue({
   router,
